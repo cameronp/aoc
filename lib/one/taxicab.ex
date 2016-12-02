8f5d3577f187ba90eba6207740b0093da9091fe1
Cameron Price <cameronp@gmail.com>
2016-12-01 21:44:52 -0500

    add comments

--- lib/one/taxicab.ex
+++ lib/one/taxicab.ex
@@ -4,13 +4,24 @@ defmodule One.Taxicab do
   defmodule State do
     defstruct location: {0, 0}, direction: :n, visited: %{} 
 
+    @doc """
+Creates a new `State`, at the origin, pointing north.
+    """
     def new, do: %State{}
 
+
+    @doc """
+Turns the state in the specified direction (:l or :r)
+    """
     def turn(%State{} = s, direction) do
       s
       |> Map.put(:direction, _turn(s.direction, direction))
     end
 
+    @doc """
+Walks forward the specified distance, calling the passed in abort_fun after 
+ever step to determine if it's time to bail.
+    """
     def careful_walk(%State{} = s, 0, _abort_fun), do: s
     def careful_walk(%State{} = s, distance, abort_fun) do
       new_state = step(s)
@@ -23,6 +34,9 @@ defmodule One.Taxicab do
       end
     end
     
+    @doc """
+Take one step forward.
+    """
     def step(%State{location: {x,y}, direction: :n} = s) do
       s
       |> Map.put(:location, {x, y+1})
@@ -65,12 +79,18 @@ defmodule One.Taxicab do
 
   alias One.Taxicab.State
 
+  @doc """
+Run part one
+  """
   def part1 do
     load
     |> run(State.new, &no_abort/1)
     |> distance
   end
 
+  @doc """
+Run part two.
+  """
   def part2 do
     load
     |> run(State.new, &visited?/1)
@@ -78,28 +98,52 @@ defmodule One.Taxicab do
   end
 
 
-  def visited?(%State{location: l, visited: v}=s) do 
-    v[l]
-  end
+  @doc """
+Determines if the current location has been visited before.  Returns true if it has
+nil otherwise.  Must be called prior to calling `visit`
 
+This is used as the abort function in part two.
+  """
+  def visited?(%State{location: l, visited: v}=s), do: v[l]
+
+  @doc """
+An abort function that never aborts.  Used for part one.
+  """
   def no_abort(_s), do: false
 
+  @doc """
+  Load and parse the given file, or 'one/input.txt' if no filename is provided.
+  """
   def load(file \\ "data/one/input.txt") do
     file
     |> readfile
     |> parse 
   end
 
+  @doc """
+parse the input file, which consists of a single line of comma separated commands.
+  """
   def parse(s) do
     s
     |> split_and_map(", ", &parse_command/1)
   end
 
+  @doc """
+parse an individual command.
+  """
   def parse_command("L" <> tail), do: %{turn: :l, distance: to_int(tail)}
   def parse_command("R" <> tail), do: %{turn: :r, distance: to_int(tail)}
 
+  @doc """
+compute the distance from the origin to the location using taxicab geometry.
+https://en.wikipedia.org/wiki/Taxicab_geometry
+  """
   def distance({x, y}), do: abs(x) + abs(y)
 
+  @doc """
+Given a state and a list of commands, runs the commands reducing the state until either
+there are no further commands, or the given abort_fun returns true.
+  """
   def run([], %State{location: location}, _abort_fun), do: location 
   def run([command | tail], %State{} = s, abort_fun) do
     case execute(s, command, abort_fun) do
@@ -109,6 +153,9 @@ defmodule One.Taxicab do
   end
 
 
+  @doc """
+Execute a single command.
+  """
   def execute(%State{} = s, command, abort_fun) do
     s
     |> State.turn(command.turn)
--- lib/utils/parsing.ex
+++ lib/utils/parsing.ex
@@ -1,16 +1,28 @@
 defmodule Utils.Parsing do
-  
+@moduledoc """
+Helper functions to assist in parsing input data.
+"""  
 
+@doc """
+Takes a path, returns a binary.  Easier for pipelining.
+"""
   def readfile(f) do
     {:ok, s} = File.read(f)
     s
   end
 
+@doc """
+Optimistically parse an integer
+"""
   def to_int(s) do
     {i, ""} = Integer.parse(s) 
     i
   end
 
+  @doc """
+Splits a binary on the specified delimeter, strips the resulting binaries, and calls
+the passed in function over them.
+  """
   def split_and_map(s, delimiter, fun) do
     s
     |> String.split(delimiter, trim: true)
